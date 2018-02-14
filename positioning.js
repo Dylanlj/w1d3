091299d3e42a595ec069eb6bcf8167cb0c46b545
Dylanlj <dylanl-j@hotmail.ca>
2018-02-14 14:50:26 +0000

    updated variable names

--- positioning.js
+++ positioning.js
@@ -1,13 +1,13 @@
 function countLetters(line){
-  var numberCount = {};
+  var letterPositioning = {};
   for(var i = 0; i < line.length; i++){
-    if(typeof numberCount[line[i]] === "object"){
-      numberCount[line[i]].push(i);
+    if(typeof letterPositioning[line[i]] === "object"){
+      letterPositioning[line[i]].push(i);
       continue;
     }
-    numberCount[line[i]] = [i];
+    letterPositioning[line[i]] = [i];
   } 
-  delete numberCount[' ']
-  return numberCount;
+  delete letterPositioning[' ']
+  return letterPositioning;
 }
 console.log(countLetters("lighthouse in the house"));
