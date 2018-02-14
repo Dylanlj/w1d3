2e24b2673034e6cb3984d27f75604e09b6574987
Dylanlj <dylanl-j@hotmail.ca>
2018-02-14 01:49:28 +0000

    inefficient, and still has commas but working

--- counting.js
+++ counting.js
@@ -1,1 +1,19 @@
-//stuff
+function countLetters(line){
+  var numberCount = {};
+  var string = line.split(' ');
+  string = string.join();
+  console.log(string);
+
+  for(var i = 0; i < string.length; i++){
+    numberCount[string[i]] = 0;
+  } 
+
+  for(var i = 0; i < string.length; i++){
+    if()
+    numberCount[string[i]] += 1;
+
+    
+  } 
+  return numberCount;
+}
+console.log(countLetters("lighthouse in the house"));
