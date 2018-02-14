0cbedccb8ddf06985e023fced5c53d76c1c66761
Dylanlj <dylanl-j@hotmail.ca>
2018-02-14 06:23:28 +0000

    everything is solved but removing commas and spaces

--- counting.js
+++ counting.js
@@ -5,15 +5,14 @@ function countLetters(line){
   console.log(string);
 
   for(var i = 0; i < string.length; i++){
-    numberCount[string[i]] = 0;
-  } 
-
-  for(var i = 0; i < string.length; i++){
-    if()
-    numberCount[string[i]] += 1;
-
-    
+     if(numberCount[string[i]] >= 1){
+        numberCount[string[i]] += 1;
+        continue;
+      }
+      numberCount[string[i]] = 1;
   } 
   return numberCount;
 }
 console.log(countLetters("lighthouse in the house"));
+
+// now you just need to get rid of the commas
